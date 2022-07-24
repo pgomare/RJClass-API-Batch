cdc8331dfa562229e175de35fe8aa8ed3a3c8846
pgomare <pundlikgomare1015@gmail.com>
2022-07-24 12:29:43 +0530

    FIrst commit

---
+++ RjClasses-Batch-API/src/main/java/com/rjclasses/batches/RjClassesBatchAPI/RjClassesBatchApiApplication.java
@@ -0,0 +1,13 @@
+package com.rjclasses.batches.RjClassesBatchAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RjClassesBatchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RjClassesBatchApiApplication.class, args);
+	}
+
+}
---
+++ RjClasses-Batch-API/src/test/java/com/rjclasses/batches/RjClassesBatchAPI/RjClassesBatchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rjclasses.batches.RjClassesBatchAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RjClassesBatchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
